d6395487a484b83b78018e8c2bec751eb8a19d55
NAJEEBKHAN123 <najeebkhanlaku.com>
2024-05-09 14:53:44 +0500

    add

---
+++ script.js
@@ -0,0 +1,33 @@
+var inputTast = document.getElementById("inputTask");
+var inputBtn = document.getElementById("taskBtn");
+var taskContainer = document.getElementById("taskContainer");
+
+inputBtn.addEventListener("click", function(){
+    var inputVal = inputTast.value.trim();
+    if(inputVal === ""){
+        alert("Enter any text")
+        return;
+    }
+   
+
+    var li = document.createElement('li');
+    li.className = "liElement";
+    li.innerHTML =`<span class="task-text">${inputVal}</span> <button class="delete-btn">delete</button>`;
+    taskContainer.appendChild(li);
+    inputTast.value = "";
+});
+taskContainer.addEventListener("click", function(x){
+    const target = x.target;
+    const taskItem = target.closest('li');
+    if(target.classList.contains('delete-btn')){
+        taskItem.remove();
+    }
+});
+
+function clearAll(){
+    document.getElementById("taskContainer").innerHTML = "";
+}
+
+
+
+
